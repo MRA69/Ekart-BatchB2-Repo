6a8193dec2e03f968e251641d49cc49acbed2e94
MRA69 <ankitmandal510@gmail.com>
2025-08-08 02:03:10 +0530

    Ekart first commit

---
+++ src/main/java/com/ekart/batchB2/BatchB2Application.java
@@ -0,0 +1,13 @@
+package com.ekart.batchB2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchB2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchB2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekart/batchB2/BatchB2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekart.batchB2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchB2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
